efb83d5f80781f91f77eda20bcdeb7e6c35d74eb
NateBougeno <43727731+NateBougeno@users.noreply.github.com>
2020-02-01 00:18:41 -0600

    Started udemy project
    Started the monster rolodex udemy project

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+//primary exporting file for all components so its located in a single place
+
+export { CardList } from './card-list/card-list.component.jsx';
+export { Card } from './card/card.component.jsx';
